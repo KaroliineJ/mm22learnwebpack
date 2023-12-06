fba88c73f63eb33a041fe19270421cd688e1355c
Karoliine Jakovleva <karoliine.jakovleva@tptlive.ee>
2023-12-06 15:09:29 +0200

    cookie

--- src/pages/Clicker.vue
+++ src/pages/Clicker.vue
@@ -22,35 +22,41 @@
 
 <script>
 export default {
-    created(){
-        setInterval(()=> {
-            for(const upgrade in this.upgrades){
-                this.cookies = (
-                    parseFloat(this.cookies) + this.upgrades[upgrade].cps * this.upgrades[upgrade].count
-                ).toFixed(1);
-            }
-        }, 1000);
-    },
-    data(){
-        return {
-            cookies: 0,
-            upgrades: {
-               cursor: {price: 10, cps: 0.1, count: 0},
-               grandma: {price: 100, cps: 1, count: 0},
-               farm: {price: 1000, cps: 10, count: 0},
-               factory: {price: 10000, cps: 100, count: 0},
-               temple: {price: 100000, cps: 1000, count: 0},
-            }
-        }
-    },
-    methods: {
-        buyUpgrade(upgrade){
-            if(this.cookies>=upgrade.price){
-                this.cookies-=upgrade.price;
-                upgrade.price += Math.ceil(upgrade.price*0.25)         
-                upgrade.count++;
-            }
-        }
+  created() {
+    setInterval(() => {
+      for (const upgrade in this.upgrades) {
+        this.cookies = (
+          parseFloat(this.cookies) + this.upgrades[upgrade].cps * this.upgrades[upgrade].count
+        ).toFixed(1);
+      }
+    }, 1000);
+
+    // Add a setInterval function to simulate auto-clicking for Auto Clicker upgrades
+    setInterval(() => {
+      this.cookies += this.upgrades.autoClicker.cps;
+    }, 1000);
+  },
+  data() {
+    return {
+      cookies: 0,
+      upgrades: {
+        cursor: {price: 10, cps: 0.1, count: 0},
+        grandma: {price: 100, cps: 1, count: 0},
+        farm: {price: 1000, cps: 10, count: 0},
+        factory: {price: 10000, cps: 100, count: 0},
+        temple: {price: 100000, cps: 1000, count: 0},
+        autoClicker: {price: 500, cps: 5, count: 0}, // Auto Clicker upgrade
+      }
+    };
+  },
+  methods: {
+    buyUpgrade(upgrade) {
+      if (this.cookies >= upgrade.price) {
+        this.cookies -= upgrade.price;
+        upgrade.price += Math.ceil(upgrade.price * 0.25);
+        upgrade.count++;
+      }
     }
-}
+  }
+};
 </script>
